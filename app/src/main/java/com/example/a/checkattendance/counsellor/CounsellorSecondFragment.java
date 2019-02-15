43255ab6b3d878a7096bd346849b664ec1ffa57f
fireheart <839397732@qq.com>
2019-02-15 14:02:29 +0800

    some modify based on the discussion yesterday and counsellor fragment completed

---
+++ app/src/main/java/com/example/a/checkattendance/counsellor/CounsellorSecondFragment.java
@@ -0,0 +1,21 @@
+package com.example.a.checkattendance.counsellor;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a.checkattendance.R;
+
+public class CounsellorSecondFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.counsellor_second_fragment,container,false);
+        return view;
+    }
+}
